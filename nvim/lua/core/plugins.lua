c8e1985b235dc03810eb09cac99f56a4774b1379
Raphael Derouelle <raph@Rapha.localdomain>
2024-05-10 00:13:03 +0200

    Added plugins to make telescope work

--- nvim/lua/core/plugins.lua
+++ nvim/lua/core/plugins.lua
@@ -29,6 +29,6 @@ require("lazy").setup({
   },
   {
     "nvim-telescope/telescope.nvim", tag = "0.1.4",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" , "BurntSushi/ripgrep", "sharkdp/fd"}
   },
 })
